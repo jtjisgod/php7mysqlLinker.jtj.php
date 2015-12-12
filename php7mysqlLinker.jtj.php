77372ed3da474775d83e2ef7c06b4bf2ade6354f
jangtaejin <jtjsoft@naver.com>
2015-12-12 20:44:45 +0900

    second commit

---
+++ php7mysqlLinker.jtj.php
@@ -0,0 +1,17 @@
+<?
+	function mysql_connect($host, $id, $pw)	{
+		return mysqli_connect($host, $id, $pw);
+	}
+
+	function mysql_select_db($dbname, $connect)	{
+		return mysqli_select_db($connect, $dbname);
+	}
+
+	function mysql_query($sql, $connect) {
+		return mysqli_query($connect, $sql);
+	}
+
+	function mysql_fetch_array($result)	{
+		return mysqli_fetch_array($result);
+	}
+?>
